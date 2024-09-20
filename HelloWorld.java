09e8e34724e64361e360ed6ce9c557c4094b98cc
abelnumberi <abelnumberi2@gmail.com>
2024-09-20 17:00:03 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama:Christiano Abel Numberi!");
+		System.out.println("NIM:607062400102");
+		System.out.println("Kelas:D3IF-48-01");
+	}
+}
